30af00400ac4205a500aa14f2c50329d9e7c5ba4
Lukas Kempf <l.k.1234@live.de>
2016-04-10 20:14:25 +0200

    Sorted usings

--- Aufgabe 1 - Geburtstagskuchen (GUI)/MainWindow.xaml.cs
+++ Aufgabe 1 - Geburtstagskuchen (GUI)/MainWindow.xaml.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Linq;
-using System.Text;
-using Timer = System.Timers.Timer;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Newtonsoft.Json;
-using Microsoft.Win32;
-using System.IO;
+using Timer = System.Timers.Timer;
 
 namespace Aufgabe_1___Geburtstagskuchen__GUI_
 {
